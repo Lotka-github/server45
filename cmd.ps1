5f009c7050dc189e74594b5509d03b3de92ad932
Lotka-github <github@lotka.org>
2022-03-28 12:43:56 +0330

    from cmd

--- cmd.ps1
+++ cmd.ps1
@@ -1,3 +1,4 @@
+echo "start"
 Get-ChildItem -Path "C:\Drive\git\server45\repository\sofiles" -Include *.* -File -Recurse | foreach { $_.Delete()}
 Copy-Item -Path "C:\Drive\Projects\AndroidStudio\oktapus\app\build\intermediates\cmake\debug\obj\" -Destination "C:\Drive\git\server45\repository\sofiles" -Recurse -force
 git add .
