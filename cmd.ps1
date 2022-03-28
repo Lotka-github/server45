8f44c05067fa2c293bfc9e8250cd0240cd7e3d48
Lotka-github <github@lotka.org>
2022-03-28 12:48:28 +0330

    from cmd

---
+++ cmd.ps1
@@ -0,0 +1,19 @@
+echo " start replacing file"
+Get-ChildItem -Path "C:\Drive\git\server45\repository\sofiles" -Include *.* -File -Recurse | foreach { $_.Delete()}
+echo " files removed"
+Copy-Item -Path "C:\Drive\Projects\AndroidStudio\oktapus\app\build\intermediates\cmake\debug\obj\" -Destination "C:\Drive\git\server45\repository\sofiles" -Recurse -force
+echo " files copied !"
+echo " initializing server"
+echo " uploading to server"
+git add .
+git commit -m "from cmd"
+git push -u origin gh-pages
+echo " uploaded"
+echo "
+ checking again ... 
+ 
+ "
+git push -u origin gh-pages
+git status
+echo " operation completed"
+pause
