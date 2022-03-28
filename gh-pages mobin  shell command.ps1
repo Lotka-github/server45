39bd62a3192c7585b182fd2f4c3d5876288ff148
Lotka-github <github@lotka.org>
2022-03-28 12:58:09 +0330

    from cmd

--- gh-pages mobin  shell command.ps1
+++ gh-pages mobin  shell command.ps1
@@ -1,15 +1,20 @@
 echo " start replacing file"
-Start-Sleep -s 3
 Get-ChildItem -Path "C:\Drive\git\server45\repository\sofiles" -Include *.* -File -Recurse | foreach { $_.Delete()}
+Start-Sleep -s 1
 echo " files removed"
 Copy-Item -Path "C:\Drive\Projects\AndroidStudio\oktapus\app\build\intermediates\cmake\debug\obj\" -Destination "C:\Drive\git\server45\repository\sofiles" -Recurse -force
+Start-Sleep -s 1
 echo " files copied !"
+Start-Sleep -s 0.5
 echo " initializing server"
+Start-Sleep -s 0.5
 echo " uploading to server`n`n"
+Start-Sleep -s 0.5
 git add .
 git commit -m "from cmd"
 git push -u origin gh-pages
 echo " uploaded"
+Start-Sleep -s 0.5
 echo "`n`n checking server ... `n`n"
 git push -u origin gh-pages
 echo "`n getting status ... `n"
